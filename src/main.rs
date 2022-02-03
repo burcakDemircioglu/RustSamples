b08dbcd4fc0355d7aeee44d72326399be0929dce
Burcak Kam <budemirc@microsoft.com>
2022-02-03 22:08:22 +0100

    Created a global entry point

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,17 @@ mod atm;
 
 fn main() -> std::io::Result<()> {
     println!("Hello, world!");
+    println!("Select the operation to run:");
+    println!("1) ATM");
 
-    atm::atm()?;
+    let mut buffer = String::new();
+    std::io::stdin().read_line(&mut buffer)?;
+    let answer = buffer.trim();
+
+    match answer {
+        "1" => atm::atm()?,
+        _ => println!("You entered an invalid input"),
+    }
 
     Ok(())
 }
