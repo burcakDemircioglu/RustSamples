04eab3f975e9212ec8a6bcc44c6ed374b25bbe1d
Burcak Kam <budemirc@microsoft.com>
2022-02-03 22:05:21 +0100

    Refactoring

--- src/atm.rs
+++ src/atm.rs
@@ -2,6 +2,33 @@
 
 use std::collections::HashMap;
 
+pub fn atm() -> std::io::Result<()> {
+    let mut atm = Atm::init()?;
+    let mut exit = false;
+
+    while !exit {
+        println!("Select the action you want to take:");
+        println!("1) Deposit");
+        println!("2) Withdraw");
+        println!("3) See the account info");
+        println!("4) Exit");
+
+        let mut buffer = String::new();
+        std::io::stdin().read_line(&mut buffer)?;
+        let answer = buffer.trim();
+
+        match answer {
+            "1" => atm.deposit()?,
+            "2" => atm.withdraw()?,
+            "3" => atm.show_account_info()?,
+            "4" => exit = true,
+            _ => println!("You entered an invalid option"),
+        }
+    }
+
+    Ok(())
+}
+
 pub struct Atm {
     account: HashMap<String, String>,
     additional_balance_amount: f32,
--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,9 @@ mod atm;
 fn main() -> std::io::Result<()> {
     println!("Hello, world!");
 
-    let mut atm = atm::Atm::init()?;
-    let mut exit = false;
+    atm::atm()?;
 
-    while !exit {
-        println!("Select the action you want to take:");
-        println!("1) Deposit");
-        println!("2) Withdraw");
-        println!("3) See the account info");
-        println!("4) Exit");
-
-        let mut buffer = String::new();
-        std::io::stdin().read_line(&mut buffer)?;
-        let answer = buffer.trim();
-
-        match answer {
-            "1" => atm.deposit()?,
-            "2" => atm.withdraw()?,
-            "3" => atm.show_account_info()?,
-            "4" => exit = true,
-            _ => println!("You entered an invalid option"),
-        }
-    }
     Ok(())
 }
+
+
